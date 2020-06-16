1b1530ca7c6d70e7c16b5268e4fee8a3cfe6657c
Jacobo Diego Pita Hernandez <jacobodhp@me.com>
2020-06-17 00:50:21 +0100

    Initial Commit

---
+++ Follow Thing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Follow Thing
+//
+//  Created by Jacobo Diego Pita Hernandez on 17/06/2020.
+//  Copyright © 2020 Jacobo Diego Pita Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
